bc056d4d690dcafa6b97731ba1158ef2b3a486b9
KazdanP <Luke.kidd@Hotmail.co.uk>
2022-05-17 10:55:43 +0100

    Wrote empty tests

---
+++ src/test/java/RaindropsTests.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.Test;
+
+public class RaindropsTests {
+
+  @Test
+  void zeroTest() {
+
+  }
+
+  @Test
+  void negativeNumberTest() {
+
+  }
+
+  @Test
+  void noFactorsTest() {
+
+  }
+
+  @Test
+  void secondNoFactorsTest() {
+
+  }
+
+  @Test
+  void factorOfThree() {
+
+  }
+
+  @Test
+  void factorOfThreeFive() {
+
+  }
+
+  @Test
+  void factorOfThreeSeven() {
+
+  }
+
+  @Test
+  void factorOfThreeFiveSeven() {
+
+  }
+
+  @Test
+  void factorOfFive() {
+
+  }
+
+  @Test
+  void factorOfFiveSeven() {
+
+  }
+
+  @Test
+  void factorOfSeven() {
+
+  }
+}
